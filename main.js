ff536a12e2c582cffe67276cfcfb14a205de71fd
Minecodes <65566012+Minecodes@users.noreply.github.com>
2020-06-19 12:18:41 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+exports.testConfig = config => {
+  const css = `
+    ${config.css || ''}
+    .tabs_nav {
+      visibility: hidden;
+    }
+    .terms_terms {
+      margin-top: 0;
+    }
+    .header_header:hover {
+      .tabs_nav {
+        visibility: visible;
+      }
+      + .terms_terms {
+        margin-top: 34px;
+      }
+    }
+  `;
+
+  return Object.assign({}, config, { css });
+};
+
+// Enable window frame on macOS
+exports.decorateBrowserOptions = defaults =>
+  Object.assign({}, defaults, {
+    titleBarStyle: '',
+    transparent: false,
+    frame: true,
+  });
