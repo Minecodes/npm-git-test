2b5058873a8d241b35ececf2da6a18434c163cab
Minecodes <65566012+Minecodes@users.noreply.github.com>
2020-06-19 13:53:27 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
-class test {
-  say(args) {
-    console.log(args)
-  }
+const { prompt } = require('enquirer');
+ 
+const response = await prompt({
+  type: 'input',
+  name: 'username',
+  message: 'What is your username?'
+});
+ 
+exports.run = function() {
+  console.log(response)
 }
-module.exports = test
