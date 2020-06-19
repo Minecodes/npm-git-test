e850e5458e17230a47c42d8ffe301a56ec188a93
Minecodes <65566012+Minecodes@users.noreply.github.com>
2020-06-19 14:03:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
-const { prompt } = require('enquirer');
-const question = [
-  {
-    type: 'input',
-    name: 'username',
-    message: 'What is your username?'
-  },
-  {
-    type: 'password',
-    name: 'password',
-    message: 'What is your password?'
-  }
-];
+const { AutoComplete } = require('enquirer');
  
-let answers = prompt(question);
+const prompt = new AutoComplete({
+  name: 'flavor',
+  message: 'Pick your favorite flavor',
+  limit: 10,
+  initial: 2,
+  choices: [
+    'Almond',
+    'Apple',
+    'Banana',
+    'Blackberry',
+    'Blueberry',
+    'Cherry',
+    'Chocolate',
+    'Cinnamon',
+    'Coconut',
+    'Cranberry',
+    'Grape',
+    'Nougat',
+    'Orange',
+    'Pear',
+    'Pineapple',
+    'Raspberry',
+    'Strawberry',
+    'Vanilla',
+    'Watermelon',
+    'Wintergreen'
+  ]
+});
  
+prompt.run()
+  .then(answer => var run = answer)
+  .catch(console.error);
+
 exports.run = function() {
-  console.log(answers)
+  console.log(run)
 }
